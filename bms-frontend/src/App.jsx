d53ea259ab362e3b243ae594ffb5d88c7da5777f
Pooja <poojachapole28@gmail.com>
2026-01-17 01:01:11 +0530

    Initial commit

---
+++ bms-frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
