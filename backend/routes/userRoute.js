6e2e2ad078c02b7ebaebd8aa8dcc141877178621
Pooja <poojachapole28@gmail.com>
2026-01-31 15:32:30 +0530

    password checks

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const UserModel = require("../model/userModel");
 const userRouter = express.Router(); //creates a router object to handle routes for user-related operations
+const validator = require("email-validator");
 const bcrypt = require("bcryptjs");
 
 const SALT_Rounds = 12;
 
 userRouter.post("/register", async (req, res) => {
     try {
+        const isEmailValid = validator.validate(req.body.email)
+
+        if (!isEmailValid) {
+            return res.status(500).send({
+                success: false,
+                message: "Please enter a valid email"
+            })   
+        }
+
         const user = new UserModel(req.body); //creating a js object out of the class defined in userModel.js
+
+        //generating a salt and hashing the password with the salt
+        const gensalt = await bcrypt.genSalt(SALT_Rounds) 
+        const hashedpassword = await bcrypt.hash(req.body.password, gensalt) 
+
+        user.password = hashedpassword; //setting the hashed password to the user object
+
         await user.save(); //saving the user object to the database
 
         res.send({
@@ -51,7 +68,7 @@ userRouter.post("/login", async function(req, res){
         if(!isPasswordValid){
             return res.status(404).send({
                 success:false,
-                message:"No user/pass combo found"
+                message:"No email/password combo found"
             })
         }
 
